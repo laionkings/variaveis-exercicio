90a27d29830a4ecaa6ae6fb043a7ce7bedaa60b7
Laion Pereira <kingslaion@gmail.com>
2022-11-21 15:53:27 -0300

    exercicios labenu

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+let nome 
+let idade 
+
+console.log("typeof.nome");
+console.log("typeof.idade");
+
+
+console.log("Olá", nome +",você tem", idade, "anos")
+console.log(`Olá ${nome}, você tem ${idade} anos`)
+
+
+const pergunta1 = "Seu mouse é sem fio?"
+const pergunta2 = "Você está usando Mac?"
+const pergunta3 = "Está nevando?"
+
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2) 
+const resposta3 = prompt(pergunta3)
+
+
+console.log(pergunta1, "-", resposta1)
+console.log(pergunta2, "-", resposta2)
+console.log(pergunta3, "-", resposta3)
---
+++ exercicio3/script.js
@@ -0,0 +1,34 @@
+let nomeCompleto = prompt("Qual seu nome completo?");
+let dataNasc = prompt("Qual sua data de nascimento");
+let endereco = prompt("Qual seu endereço?");
+let cpf = prompt("Qual seu CPF?");
+let escolaridade = prompt("Qual sua Escolaridade?");
+let cnh = prompt("Qual sua CNH?");
+let filhos = prompt("Quantos filhos possui?");
+let cargo = prompt("Qual seu cargo atual?");
+let salario = prompt("Qual seu salário?");
+let comissao = prompt("Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero");
+let admissao = prompt("Informe o ano de admissão");
+
+typeof nomeCompleto,
+typeof dataNasc,
+typeof endereco,
+typeof cpf,
+typeof escolaridade,
+typeof cnh,
+typeof filhos,
+typeof cargo,
+typeof salario,
+typeof comissao,
+typeof admissao;
+
+
+console.log(`${nomeCompleto}`);
+console.log(`${dataNasc}`);
+console.log(`${endereco}`);
+console.log(`${cpf}`);
+console.log(`${filhos}`);
+console.log(`${cargo}`);
+console.log(`${salario}`);
+console.log(`${comissao}`);
+console.log(`${admissao}`);
